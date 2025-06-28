1b90a382f2d9e1b244692875233cbb1114c6947c
Carlos Jorge Pereira <144349074+podefteza@users.noreply.github.com>
2025-06-28 22:46:27 +0100

    Update microshell.c

--- microshell.c
+++ microshell.c
@@ -1,78 +1,71 @@
+#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
 
-void	error(char *msg, char *arg)
+int	err(char *str)
 {
-	while (*msg) write(2, msg++, 1);
-	if (arg) while (*arg) write(2, arg++, 1);
-	write(2, "\n", 1);
+	while (*str)
+		write(2, str++, 1);
+	return (1);
 }
 
-void	exec_cmd(char **argv, int i, int tmp_fd, char **env)
+int	cd(char **av, int i)
 {
-	argv[i] = NULL;
-	dup2(tmp_fd, 0);
-	close(tmp_fd);
-	execve(argv[0], argv, env);
-	error("error: cannot execute ", argv[0]);
-	exit(1);
+	if (i != 2)
+		return (err("error: cd: bad arguments\n"));
+	else if (chdir(av[1]) == -1)
+		return (err("error: cd: cannot change directory to "), err(av[1]),
+			err("\n"));
+	return (0);
 }
 
-int	main(int argc, char **argv, char **env)
+int	exec(char **av, char **env, int i)
 {
-	int	i = 0, fd[2], tmp_fd = dup(0);
-	(void)argc;
+	int	fd[2];
+	int	status;
+	int	has_pipe;
+	int	pid;
 
-	while (argv[i] && argv[i + 1])
+	has_pipe = av[i] && !strcmp(av[i], "|");
+	if (!has_pipe && !strcmp(*av, "cd"))
+		return (cd(av, i));
+	if (has_pipe && pipe(fd) == -1)
+		return (err("error: fatal\n"));
+	pid = fork();
+	if (!pid)
 	{
-		argv += i + 1;
-		for (i = 0; argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"); i++);
+		av[i] = 0;
+		if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+			return (err("error: fatal\n"));
+		if (!strcmp(*av, "cd"))
+			return (cd(av, i));
+		execve(*av, av, env);
+		return (err("error: cannot execute "), err(*av), err("\n"));
+	}
+	waitpid(pid, &status, 0);
+	if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+		return (err("error: fatal\n"));
+	return (WIFEXITED(status) && WEXITSTATUS(status));
+}
 
-		if (!strcmp(argv[0], "cd"))
+int	main(int ac, char **av, char **env)
+{
+	int	i;
+	int	status;
+
+	i = 0;
+	status = 0;
+	if (ac > 1)
+	{
+		while (av[i] && av[++i])
 		{
-			if (i != 2) error("error: cd: bad arguments", NULL);
-			else if (chdir(argv[1])) error("error: cd: cannot change directory to ", argv[1]);
-		}
-		else if (i != 0 && (!argv[i] || !strcmp(argv[i], ";")))
-		{
-			if (!fork())
-			{
-				exec_cmd(argv, i, tmp_fd, env);
-				close(tmp_fd);
-			}
-			else
-			{
-				close(tmp_fd);
-				while (waitpid(-1, NULL, 2) != -1);
-				tmp_fd = dup(0);
-			}
-		}
-		else if (i != 0 && (!argv[i] || !strcmp(argv[i], "|")))
-		{
-			pipe(fd);
-			int pid = fork();
-			if (pid == 0)
-			{
-				dup2(fd[1], 1);
-				close(fd[0]);
-				close(fd[1]);
-				exec_cmd(argv, i, tmp_fd, env);
-				close(tmp_fd);
-				exit(1);
-			}
-			else
-			{
-				close(fd[1]);
-				close(tmp_fd);
-				tmp_fd = fd[0];
-				waitpid(pid, NULL, 0);
-			}
-			close(fd[0]);
+			av += i;
+			i = 0;
+			while (av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
+				i++;
+			if (i)
+				status = exec(av, env, i);
 		}
 	}
-	close(tmp_fd);
-	return (0);
+	return (status);
 }
